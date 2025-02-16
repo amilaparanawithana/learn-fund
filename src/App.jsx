e4dc3b9ce919bff55f3a98a257174005fc4a195d
amilaparanawithana <amila@huubap.com>
2025-02-17 11:47:52 +1300

    conditional data display

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,28 @@ import {useState} from 'react';
 const reactFundermentals = ['hello']
 
 function App() {
-       const [selectedButton, setSelectedButton] = useState('components');
+    const [selectedButton, setSelectedButton] = useState('');
 
     function handleButtonClick(selectedButton) {
         setSelectedButton(selectedButton);
         console.log(selectedButton);
     }
 
+    let tabContent = <p>Please select a tab</p>
+
+    if (selectedButton) {
+        tabContent = (
+            <div id="tab-content">
+                <h3>{EXAMPLES[selectedButton].title}</h3>
+                <p>{EXAMPLES[selectedButton].description}</p>
+                <pre>
+                        <code>
+                    {EXAMPLES[selectedButton].code}
+                        </code>
+                        </pre>
+            </div>
+        );
+    }
     return (
         <div>
             <Header/>
@@ -22,14 +37,16 @@ function App() {
                 <section id="core-concepts">
                     <h3>Core Concepts</h3>
                     <ul>
-                        <CoreConcepts
+                        {CORE_CONCEPTS.map(conceptItem => <CoreConcepts key = {conceptItem.title} {...conceptItem}/>)}
+                        {/*<CoreConcepts
                             title="components1"
                             description="description2"
                             image={componentsImg}
                         ></CoreConcepts>
                         <CoreConcepts {...CORE_CONCEPTS[0]}/>
                         <CoreConcepts {...CORE_CONCEPTS[1]}/>
-                        <CoreConcepts {...CORE_CONCEPTS[2]}/>
+                        <CoreConcepts {...CORE_CONCEPTS[2]}/>*/}
+
                         {/*<CoreConcepts concept = {CORE_CONCEPTS[1]}/>*/}
                     </ul>
                 </section>
@@ -37,23 +54,30 @@ function App() {
 
                     <h2>Examples</h2>
                     <menu>
-                        <TabButton onSelect={()=> handleButtonClick('components')}>Components</TabButton> {/*or can pass the text as a attribute*/}
-                        <TabButton onSelect={()=> handleButtonClick('jsx')}>JX</TabButton> {/*or can pass the text as a attribute*/}
-                        <TabButton onSelect={()=> handleButtonClick('props')}>Props</TabButton> {/*or can pass the text as a attribute*/}
-                        <TabButton onSelect={()=> handleButtonClick('state')}>State</TabButton> {/*or can pass the text as a attribute*/}
+                        <TabButton isSelected={selectedButton === 'components'}
+                            onSelect={() => handleButtonClick('components')}>Components</TabButton> {/*or can pass the text as a attribute*/}
+                        <TabButton isSelected={selectedButton === 'jsx'}
+                            onSelect={() => handleButtonClick('jsx')}>JX</TabButton> {/*or can pass the text as a attribute*/}
+                        <TabButton isSelected={selectedButton === 'props'}
+                            onSelect={() => handleButtonClick('props')}>Props</TabButton> {/*or can pass the text as a attribute*/}
+                        <TabButton isSelected={selectedButton === 'state'}
+                            onSelect={() => handleButtonClick('state')}>State</TabButton> {/*or can pass the text as a attribute*/}
                     </menu>
                 </section>
                 <h2>{selectedButton}</h2>
-                <div id = "tab-content">
-                    <h3>{EXAMPLES[selectedButton].title}</h3>
-                    <p>{EXAMPLES[selectedButton].description}</p>
-                    <pre>
-                        <code>
-                           {EXAMPLES[selectedButton].code}
-                        </code>
-                    </pre>
 
-                </div>
+                {tabContent}
+                {/*{!selectedButton ? <p>Please select a topic</p> :
+                    <div id="tab-content">
+                        <h3>{EXAMPLES[selectedButton].title}</h3>
+                        <p>{EXAMPLES[selectedButton].description}</p>
+                        <pre>
+                        <code>
+                    {EXAMPLES[selectedButton].code}
+                        </code>
+                        </pre>
+                    </div>}*/}
+
             </main>
         </div>
     );
